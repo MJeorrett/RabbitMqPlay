a1701a9d6fc6fa168fb07661b707986da08bcbcf
Matthew Jeorrett <matthew@clearskylogic.com>
2025-11-12 23:26:17 +0000

    Add project files.

---
+++ RabbitMqPlay.PublisherApi/Controllers/PublisherController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMqPlay.PublisherApi.Controllers
+{
+    [ApiController]
+    public class PublisherController
+    {
+        public record PublishRequest
+        {
+            public required string Message { get; init; }
+        }
+
+        [HttpPost("/publish")]
+        public async Task<IActionResult> PublishMessage(
+            [FromBody] PublishRequest request,
+            IConnection connection)
+        {
+            using var channel = await connection.CreateChannelAsync();
+            await channel.QueueDeclareAsync(queue: "hello",
+                                            durable: false,
+                                            exclusive: false,
+                                            autoDelete: false,
+                                            arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(request.Message);
+
+            await channel.BasicPublishAsync(
+                exchange: string.Empty,
+                routingKey: "hello",
+                body: body);
+
+            return new OkObjectResult("Message published");
+        }
+    }
+}
